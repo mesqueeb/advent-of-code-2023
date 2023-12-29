4d75c7ebde2971584fb14901950b3228695c37e0
mesqueeb <mesqueeb@users.noreply.github.com>
2023-12-29 12:03:35 -0600

    day 1

---
+++ days/01/part1.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+// Get the directory of the current script file
+let scriptDir = URL(fileURLWithPath: #file).deletingLastPathComponent()
+
+// Assuming the file is in the current directory
+let fileName = "data.txt"
+
+// Construct the file URL
+let fileURL = scriptDir.appendingPathComponent(fileName)
+
+do {
+	// Read the content of the file
+	let content = try String(contentsOf: fileURL, encoding: .utf8)
+
+	// Split the content into lines
+	let lines = content.components(separatedBy: .newlines)
+
+	var total = 0
+
+	for line in lines {
+		let digits = line.compactMap { Int(String($0)) }
+		let firstDigit = digits.first ?? 0
+		let lastDigit = digits.last ?? 0
+		let number = Int(String("\(firstDigit)\(lastDigit)")) ?? 0
+		total += number
+	}
+	print("total: \(total)")
+} catch {
+	print("Error reading the file: \(error)")
+}
