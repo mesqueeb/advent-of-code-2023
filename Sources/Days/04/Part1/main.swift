109153f10fb08a45a7b9f0464fd9b13168a7745b
mesqueeb <mesqueeb@users.noreply.github.com>
2024-01-09 11:50:37 -0600

    day 4 ⭐️⭐️

--- Sources/Days/04/Part1/main.swift
+++ Sources/Days/04/Part1/main.swift
@@ -5,3 +5,32 @@ let data = readFile(filePath: "../data.txt")
 
 print(data)
 
+// Split the content into lines
+let lines = data.components(separatedBy: .newlines)
+
+var total = 0
+
+for line in lines {
+	var cardScore = 0
+
+	let parts = line.components(separatedBy: CharacterSet(charactersIn: "|:")).map { $0.trimmingCharacters(in: .whitespaces) }
+	guard parts.count >= 3 else {
+		continue
+	}
+	let card = parts[1]
+	let ownNumbers = parts[2]
+	let cardNumbers = card.split(separator: " ").compactMap { Int($0) }
+	let winningNumbers = ownNumbers.split(separator: " ").compactMap { Int($0) }
+	for cardNumber in cardNumbers {
+		if winningNumbers.contains(cardNumber) {
+			if cardScore == 0 {
+				cardScore = 1
+			} else {
+				cardScore *= 2
+			}
+		}
+	}
+	total += cardScore
+}
+
+print("total →", total)
--- Sources/Days/04/Part2/main.swift
+++ Sources/Days/04/Part2/main.swift
@@ -1,7 +1,50 @@
 import Foundation
 import Utils
 
+let startTime = Date()
+
 let data = readFile(filePath: "../data.txt")
 
 print(data)
 
+// Split the content into lines
+let lines = data.components(separatedBy: .newlines).filter { !$0.isEmpty }
+
+var cardIndexCopyCountDic: [Int: Int] = [:]
+
+for (lineIndex, line) in lines.enumerated() {
+	let cardCopiesForThisLine = 1 + (cardIndexCopyCountDic[lineIndex] ?? 0)
+	cardIndexCopyCountDic[lineIndex] = cardCopiesForThisLine
+
+	let parts = line.components(separatedBy: CharacterSet(charactersIn: "|:")).map { $0.trimmingCharacters(in: .whitespaces) }
+	guard parts.count >= 3 else {
+		continue
+	}
+	let card = parts[1]
+	let ownNumbers = parts[2]
+	let cardNumbers = card.split(separator: " ").compactMap { Int($0) }
+	let winningNumbers = Set(ownNumbers.split(separator: " ").compactMap { Int($0) })
+
+	var cardCopiesWonFromThisLine = 0
+	for cardNumber in cardNumbers {
+		if winningNumbers.contains(cardNumber) {
+			cardCopiesWonFromThisLine += 1
+		}
+	}
+
+	if cardCopiesWonFromThisLine > 0 {
+		for _ in 1 ... cardCopiesForThisLine {
+			for copyNr in 1 ... cardCopiesWonFromThisLine {
+				cardIndexCopyCountDic[lineIndex + copyNr, default: 0] += 1
+			}
+		}
+	}
+}
+
+var total = cardIndexCopyCountDic.values.reduce(0) { total, count in total + count }
+print("total →", total)
+
+let endTime = Date()
+let timeInterval = endTime.timeIntervalSince(startTime)
+
+print("Time taken: \(timeInterval) seconds")
