d2fe2c26d2aa7ceff4a78e3c111b2137465b6285
mesqueeb <mesqueeb@users.noreply.github.com>
2024-01-08 18:29:01 -0600

    day 3 ⭐️⭐️

--- Sources/Days/03/Part1/main.swift
+++ Sources/Days/03/Part1/main.swift
@@ -3,4 +3,103 @@ import Utils
 
 let data = readFile(filePath: "../data.txt")
 
-print(data)
+typealias Number = (lineIndex: Int, value: Int, index: (from: Int, to: Int))
+typealias SymbolIndex = (lineIndex: Int, symbolIndex: Int)
+
+func lineToNumbers(_ lineIndex: Int, _ line: String) -> [Number] {
+	var result: [Number] = []
+
+	var numberStartedAt: Int = -1
+	var numberStoppedAt: Int = -1
+	for (i, character) in ("." + line + ".").enumerated() {
+		if character.isNumber {
+			if numberStartedAt == -1 {
+				numberStartedAt = i
+			}
+			numberStoppedAt = i
+		} else {
+			if numberStoppedAt != -1 {
+				let startIndex = line.index(line.startIndex, offsetBy: numberStartedAt - 1)
+				let endIndex = line.index(line.startIndex, offsetBy: numberStoppedAt - 1)
+				let numberString = String(line[startIndex ... endIndex])
+				guard let numberValue = Int(numberString) else {
+					print("wrong number parsing logic", numberString)
+					return result
+				}
+
+				result.append(
+					Number(
+						lineIndex: lineIndex,
+						value: numberValue,
+						index: (from: numberStartedAt, to: numberStoppedAt)
+					)
+				)
+			}
+			// let's reset
+			numberStartedAt = -1
+			numberStoppedAt = -1
+		}
+	}
+	return result
+}
+
+func lineToSymbolIndexes(_ lineIndex: Int, _ line: String) -> [SymbolIndex] {
+	var result: [SymbolIndex] = []
+
+	for (i, character) in ("." + line + ".").enumerated() {
+		if character == "." { continue }
+		let number = Int(String(character))
+		if number != nil { continue }
+
+		result.append(SymbolIndex(lineIndex: lineIndex, symbolIndex: i))
+	}
+
+	return result
+}
+
+func numberToPossibleSymbolIndexes(_ number: Number) -> [SymbolIndex] {
+	var result: [SymbolIndex] = []
+
+	let from = number.index.from
+	let to = number.index.to
+
+	let rangeArray = Array((from - 1) ... (to + 1))
+
+	for possibileIndex in rangeArray {
+		result.append(SymbolIndex(lineIndex: number.lineIndex - 1, symbolIndex: possibileIndex))
+	}
+	for possibileIndex in rangeArray {
+		result.append(SymbolIndex(lineIndex: number.lineIndex, symbolIndex: possibileIndex))
+	}
+	for possibileIndex in rangeArray {
+		result.append(SymbolIndex(lineIndex: number.lineIndex + 1, symbolIndex: possibileIndex))
+	}
+
+	return result
+}
+
+// Split the content into lines
+let lines = data.components(separatedBy: .newlines)
+
+var total = 0
+
+var allSymbolIndexes: [SymbolIndex] = []
+var allNumbers: [Number] = []
+
+for (lineIndex, line) in lines.enumerated() {
+	let lineNumbers = lineToNumbers(lineIndex, line)
+	allNumbers.append(contentsOf: lineNumbers)
+	let lineSymbolIndexes = lineToSymbolIndexes(lineIndex, line)
+	allSymbolIndexes.append(contentsOf: lineSymbolIndexes)
+}
+
+for number in allNumbers {
+	let possibleSymbolIndexes = numberToPossibleSymbolIndexes(number)
+	for symbolIndex in allSymbolIndexes {
+		if possibleSymbolIndexes.contains(where: { possibleSymbolIndex in symbolIndex.lineIndex == possibleSymbolIndex.lineIndex && symbolIndex.symbolIndex == possibleSymbolIndex.symbolIndex }) {
+			total += number.value
+		}
+	}
+}
+
+print("total: \(total)")
--- Sources/Days/03/Part2/main.swift
+++ Sources/Days/03/Part2/main.swift
@@ -3,4 +3,107 @@ import Utils
 
 let data = readFile(filePath: "../data.txt")
 
-print(data)
+typealias Number = (lineIndex: Int, value: Int, index: (from: Int, to: Int))
+typealias GearIndex = (lineIndex: Int, gearIndex: Int)
+
+func lineToNumbers(_ lineIndex: Int, _ line: String) -> [Number] {
+	var result: [Number] = []
+
+	var numberStartedAt: Int = -1
+	var numberStoppedAt: Int = -1
+	for (i, character) in ("." + line + ".").enumerated() {
+		if character.isNumber {
+			if numberStartedAt == -1 {
+				numberStartedAt = i
+			}
+			numberStoppedAt = i
+		} else {
+			if numberStoppedAt != -1 {
+				let startIndex = line.index(line.startIndex, offsetBy: numberStartedAt - 1)
+				let endIndex = line.index(line.startIndex, offsetBy: numberStoppedAt - 1)
+				let numberString = String(line[startIndex ... endIndex])
+				guard let numberValue = Int(numberString) else {
+					print("wrong number parsing logic", numberString)
+					return result
+				}
+
+				result.append(
+					Number(
+						lineIndex: lineIndex,
+						value: numberValue,
+						index: (from: numberStartedAt, to: numberStoppedAt)
+					)
+				)
+			}
+			// let's reset
+			numberStartedAt = -1
+			numberStoppedAt = -1
+		}
+	}
+	return result
+}
+
+func lineToGearIndexes(_ lineIndex: Int, _ line: String) -> [GearIndex] {
+	var result: [GearIndex] = []
+
+	for (i, character) in ("." + line + ".").enumerated() {
+		if character == "." { continue }
+		let number = Int(String(character))
+		if number != nil { continue }
+
+		result.append(GearIndex(lineIndex: lineIndex, gearIndex: i))
+	}
+
+	return result
+}
+
+func numberToPossibleGearIndexes(_ number: Number) -> [GearIndex] {
+	var result: [GearIndex] = []
+
+	let from = number.index.from
+	let to = number.index.to
+
+	let rangeArray = Array((from - 1) ... (to + 1))
+
+	for possibileIndex in rangeArray {
+		result.append(GearIndex(lineIndex: number.lineIndex - 1, gearIndex: possibileIndex))
+	}
+	for possibileIndex in rangeArray {
+		result.append(GearIndex(lineIndex: number.lineIndex, gearIndex: possibileIndex))
+	}
+	for possibileIndex in rangeArray {
+		result.append(GearIndex(lineIndex: number.lineIndex + 1, gearIndex: possibileIndex))
+	}
+
+	return result
+}
+
+// Split the content into lines
+let lines = data.components(separatedBy: .newlines)
+
+var total = 0
+
+var allGearIndexes: [GearIndex] = []
+var allNumbers: [Number] = []
+
+for (lineIndex, line) in lines.enumerated() {
+	let lineNumbers = lineToNumbers(lineIndex, line)
+	allNumbers.append(contentsOf: lineNumbers)
+	let lineGearIndexes = lineToGearIndexes(lineIndex, line)
+	allGearIndexes.append(contentsOf: lineGearIndexes)
+}
+
+for gearIndex in allGearIndexes {
+	var numbersAdjacent: [Number] = []
+	for number in allNumbers {
+		let possibleGearIndexes = numberToPossibleGearIndexes(number)
+		if possibleGearIndexes.contains(where: { possibleGearIndex in gearIndex.lineIndex == possibleGearIndex.lineIndex && gearIndex.gearIndex == possibleGearIndex.gearIndex }) {
+			numbersAdjacent.append(number)
+		}
+	}
+	if numbersAdjacent.count == 2 {
+		total += numbersAdjacent[0].value * numbersAdjacent[1].value
+	}
+}
+
+print("total: \(total)")
